722450fa4940962d1511de2f9646eee47b995496
leonelmiguins <leomiguins@gmail.com>
2023-03-16 16:39:30 -0300

    update

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # Sobre o bot
 
-Este é um bot projetado para gerenciar um grupo do GitHub que visa promover a colaboração entre desenvolvedores, estudantes e entusiastas da tecnologia.<br>
-O bot ajudará a facilitar a interação dos membros do grupo, permitindo que eles compartilhem seus projetos no GitHub, se conectem através do LinkedIn e criem oportunidades de networking.<br>
+<p>Este é um bot projetado para gerenciar um grupo do GitHub que visa promover a colaboração entre desenvolvedores, estudantes e entusiastas da tecnologia. O bot ajudará a facilitar a interação dos membros do grupo, permitindo que eles compartilhem seus projetos no GitHub, se conectem através do LinkedIn e criem oportunidades de networking.</p>
 
 # funcionalidades
 
-Verificar automaticamente se os novos membros do grupo já compartilharam seus perfis do GitHub e do LinkedIn.<br>
-Monitorar o grupo em busca de novas postagens e fornecer notificações aos membros relevantes.<br>
-Incentivar os membros a compartilharem seus projetos no GitHub e fornecer feedbacks uns aos outros.<br>
-Permitir que os membros solicitem ajuda em seus projetos e incentivem a colaboração.<br>
-Promover eventos e iniciativas relevantes relacionados a tecnologia e desenvolvimento de software.<br>
+✅ Verificar automaticamente se os novos membros do grupo já compartilharam seus perfis do GitHub e do LinkedIn.<br>
+✅ Monitorar o grupo em busca de novas postagens e fornecer notificações aos membros relevantes.<br>
+✅ Incentivar os membros a compartilharem seus projetos no GitHub e fornecer feedbacks uns aos outros.<br>
+✅ Permitir que os membros solicitem ajuda em seus projetos e incentivem a colaboração.<br>
+✅ Promover eventos e iniciativas relevantes relacionados a tecnologia e desenvolvimento de software.<br>
--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');
+const console = require('console');
 
 const client = new Client({
     authStrategy: new LocalAuth(),
@@ -17,8 +18,7 @@ client.on('qr', (qr) => {
 client.on('authenticated', () => {
     console.log('Sessão Autenticada!');
 });
-
-  
+ 
 client.on('ready', () => {
     console.log(' rodando!');
 });
@@ -29,44 +29,71 @@ client.on('group_join', (notification) => {
     notification.reply(wellcome);
 });
 
-//menssagem de saida do grupo
-    client.on('group_leave', (notification) => {
-    console.log('--» membro saiu'); 
-    let bye = readFile('./menu/bye.txt')
-    notification.reply(bye);
-});
-
 
 client.on('message', async (msg) => {
 
     if(msg.body === '$menu' || msg.body === '$start'){
         let data = readFile('./menus/main.txt')
         msg.reply(data);
-
     }
 
     if(msg.body === '$rules'){
         let data = readFile('./menus/rules.txt')
         msg.reply(data);
-
     }
 
     if(msg.body === '$about'){
         let data = readFile('./menus/about.txt')        
         msg.reply(data);
-
     }
 
     if(msg.body === '$gits'){
         let data = readFile('./menus/githubs.txt')        
         msg.reply(data);
-
     }
 
     if(msg.body === '$proj'){
         let data = readFile('./menus/projects.txt')        
         msg.reply(data);
+    }
 
+    if(msg.body === '$add-git'){ 
+        let data = readFile('./menus/add-git.txt')        
+        msg.reply(data);
+    
+    }
+
+    if(msg.body.includes('https://github.com/') && msg.body.includes('$add-git')){
+        let tel = ((await msg.getContact()).id._serialized).substring(0, 12);
+        let link = msg.body.substring(9);
+        let data = readFile('./menus/githubs.txt');
+
+        if(data.includes(link)){
+          msg.reply('*⚠ Esse perfil já está cadastrado em nossa base de dados!*')
+
+        }else{
+            let username = msg.body.substring(28)
+            data = data+`\n ➥ user: *${username}*\ngit: ${link} tel: *${tel}*`;
+            writeFile('./menus/githubs.txt', data);
+            msg.reply('*✅ Perfil adicionado com sucesso!*')
+        }
+
+    }
+
+    if (msg.body.includes('https://wa.me/') || msg.body.includes('chat.whatsapp.com/')) {
+
+       //excluir menssagens de membros que mandam links de outros canais
+       client.sendMessage(msg.from, '*⚠ Proibido enviar links de grupos sem permissão !*');
+       msg.delete(true);
+
+       //banindo quem enviou o link
+       const membro_id = (await msg.getContact()).id._serialized
+       console.log(membro_id)
+       let chat = await msg.getChat();
+       let chat_id = chat.id._serialized;
+ 
+       chat = await client.getChatById(chat_id);
+       await chat.removeParticipants([membro_id]);
     }
 
 });
@@ -78,3 +105,11 @@ function readFile(file){
    
      return dados;
   }
+
+function writeFile(arq, data) {
+
+fs.writeFile(arq, data, (err) => {
+    if (err) throw err;
+  console.log('O arquivo foi criado!');
+});
+}
